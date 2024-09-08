cb03fd3823cd146acbf52c531f15ec977688e61d
Abdul <hafizhahm123@gmail.com>
2024-09-08 09:44:19 +0700

    init: eslint & port

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-console.log('halooooooooooooooooooo');
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: 'localhost',
+  });
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
